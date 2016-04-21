192d5084d3cd685c14063d60cb7433a5295ba629
johnlonganecker <johnlonganecker@gmail.com>
2016-04-21 16:32:42 -0700

    add support for testing stomp and mqtt

--- service/service_test.go
+++ service/service_test.go
@@ -46,9 +46,13 @@ var config = loadConfig()
 var context services.Context
 
 var _ = Describe("RabbitMQ Service", func() {
-	var timeout = time.Second * 60
-	var retryInterval = time.Second * 1
-	var appPath = "../assets/cf-rabbitmq-example-app"
+	var (
+		timeout       = time.Second * 60
+		retryInterval = time.Second * 1
+		appAMQPPath   = "../assets/cf-rabbitmq-example-app"
+		appMQTTPath   = "../assets/cf-rabbitmq-example-mqtt-app"
+		appSTOMPPath  = "../assets/cf-rabbitmq-example-stomp-app"
+	)
 
 	var appName string
 
@@ -77,7 +81,7 @@ var _ = Describe("RabbitMQ Service", func() {
 		context.Teardown()
 	})
 
-	AssertLifeCycleBehavior := func(planName string) {
+	AssertLifeCycleAMQPBehavior := func(planName, planPath string) {
 		var serviceInstanceName string
 		appPushed := false
 		serviceCreated := false
@@ -156,9 +160,148 @@ var _ = Describe("RabbitMQ Service", func() {
 		})
 	}
 
+	AssertLifeCycleMQTTBehavior := func(planName, planPath string) {
+		var serviceInstanceName string
+		appPushed := false
+		serviceCreated := false
+		serviceBound := false
+		appIsRunning := false
+
+		It("Should be able to push the application", func() {
+			appName = randomName()
+			Eventually(cf.Cf("push", appName, "-m", "256M", "-p", appPath, "-s", "cflinuxfs2", "-no-start"), config.ScaledTimeout(timeout)).Should(Exit(0))
+			appPushed = true
+		})
+
+		It("Can create the service instance", func() {
+			Ω(appPushed).Should(BeTrue())
+			serviceInstanceName = randomName()
+			Eventually(cf.Cf("create-service", config.ServiceName, planName, serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+			serviceCreated = true
+		})
+
+		It("Can bind the service and start the application", func() {
+			Ω(appPushed && serviceCreated).Should(BeTrue())
+			Eventually(cf.Cf("bind-service", appName, serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+			serviceBound = true
+			var RMQSkipSSLValue string = "0"
+			if config.RabbitMQSkipSSL {
+				RMQSkipSSLValue = "1"
+			}
+			Eventually(cf.Cf("set-env", appName, "RABBITMQ_SKIP_SSL", RMQSkipSSLValue), config.ScaledTimeout(5*time.Minute)).Should(Exit(0))
+			Eventually(cf.Cf("start", appName), config.ScaledTimeout(5*time.Minute)).Should(Exit(0))
+			assertAppIsRunning(appName)
+			appIsRunning = true
+		})
+
+		It("can write to and read from a service instance using the "+planName+" plan", func() {
+			Ω(appPushed && serviceCreated && serviceBound && appIsRunning).Should(BeTrue())
+			/*
+			   subscribe          (should 204)
+			   publish            (should 201)
+			   subscribe          (should 200)
+			*/
+
+			uri = appUri(appName) + "/queue/test-q"
+			fmt.Println("Reading from the (empty) queue: ", uri)
+			Eventually(runner.Curl(uri, "-k"), config.ScaledTimeout(timeout), retryInterval).Should(Say(""))
+			fmt.Println("\n")
+
+			fmt.Println("Publishing to the queue: ", uri)
+			Eventually(runner.Curl("-d", "data=test-message", "-X", "PUT", uri, "-k"), config.ScaledTimeout(timeout), retryInterval).Should(Say("SUCCESS"))
+			fmt.Println("\n")
+
+			fmt.Println("Reading from the (non-empty) queue: ", uri)
+			Eventually(runner.Curl(uri, "-k"), config.ScaledTimeout(timeout), retryInterval).Should(Say("test-message"))
+			fmt.Println("\n")
+		})
+
+		It("Should be able to clean up after itself", func() {
+			if serviceBound {
+				Eventually(cf.Cf("unbind-service", appName, serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+			}
+			if serviceCreated {
+				Eventually(cf.Cf("delete-service", "-f", serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+			}
+			if appPushed {
+				Eventually(cf.Cf("delete", appName, "-f"), config.ScaledTimeout(timeout)).Should(Exit(0))
+			}
+		})
+	}
+
+	AssertLifeCycleSTOMPBehavior := func(planName, planPath string) {
+		var serviceInstanceName string
+		appPushed := false
+		serviceCreated := false
+		serviceBound := false
+		appIsRunning := false
+
+		It("Should be able to push the application", func() {
+			appName = randomName()
+			Eventually(cf.Cf("push", appName, "-m", "256M", "-p", appPath, "-s", "cflinuxfs2", "-no-start"), config.ScaledTimeout(timeout)).Should(Exit(0))
+			appPushed = true
+		})
+
+		It("Can create the service instance", func() {
+			Ω(appPushed).Should(BeTrue())
+			serviceInstanceName = randomName()
+			Eventually(cf.Cf("create-service", config.ServiceName, planName, serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+			serviceCreated = true
+		})
+
+		It("Can bind the service and start the application", func() {
+			Ω(appPushed && serviceCreated).Should(BeTrue())
+			Eventually(cf.Cf("bind-service", appName, serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+			serviceBound = true
+			var RMQSkipSSLValue string = "0"
+			if config.RabbitMQSkipSSL {
+				RMQSkipSSLValue = "1"
+			}
+			Eventually(cf.Cf("set-env", appName, "RABBITMQ_SKIP_SSL", RMQSkipSSLValue), config.ScaledTimeout(5*time.Minute)).Should(Exit(0))
+			Eventually(cf.Cf("start", appName), config.ScaledTimeout(5*time.Minute)).Should(Exit(0))
+			assertAppIsRunning(appName)
+			appIsRunning = true
+		})
+
+		It("can write to and read from a service instance using the "+planName+" plan", func() {
+			Ω(appPushed && serviceCreated && serviceBound && appIsRunning).Should(BeTrue())
+			/*
+			   subscribe          (should 204)
+			   publish            (should 201)
+			   subscribe          (should 200)
+			*/
+			uri = appUri(appName) + "/queue/test-q"
+			fmt.Println("Reading from the (empty) queue: ", uri)
+			Eventually(runner.Curl(uri, "-k"), config.ScaledTimeout(timeout), retryInterval).Should(Say(""))
+			fmt.Println("\n")
+
+			fmt.Println("Publishing to the queue: ", uri)
+			Eventually(runner.Curl("-d", "data=test-message", "-X", "PUT", uri, "-k"), config.ScaledTimeout(timeout), retryInterval).Should(Say("SUCCESS"))
+			fmt.Println("\n")
+
+			fmt.Println("Reading from the (non-empty) queue: ", uri)
+			Eventually(runner.Curl(uri, "-k"), config.ScaledTimeout(timeout), retryInterval).Should(Say("test-message"))
+			fmt.Println("\n")
+		})
+
+		It("Should be able to clean up after itself", func() {
+			if serviceBound {
+				Eventually(cf.Cf("unbind-service", appName, serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+			}
+			if serviceCreated {
+				Eventually(cf.Cf("delete-service", "-f", serviceInstanceName), config.ScaledTimeout(timeout)).Should(Exit(0))
+			}
+			if appPushed {
+				Eventually(cf.Cf("delete", appName, "-f"), config.ScaledTimeout(timeout)).Should(Exit(0))
+			}
+		})
+	}
+
 	Context("for each plan", func() {
 		for _, planName := range config.PlanNames {
-			AssertLifeCycleBehavior(planName)
+			AssertLifeCycleAMQPBehavior(planName, planAMQPPath)
+			AssertLifeCycleMQTTBehavior(planName, planMQTTPath)
+			AssertLifeCycleSTOMPBehavior(planName, PlanSTOMPPath)
 		}
 	})
 })
